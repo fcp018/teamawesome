208321f6405235ef0d4b59ccfc0e15e1e57e3a7d
rsangole <rsangole@users.noreply.github.com>
2017-07-25 00:50:07 -0400

    Creates twitter hashtag lists

---
+++ twitter_hashtags.R
@@ -0,0 +1,37 @@
+# Seattle Brunch Run
+brunchrun_tags <- c('#BrunchRunSea')
+#Relevent user - @BrunchRunSea
+
+# Warrior Dash
+warriordash_tags <- c('#WarriorDash AND #Seattle',
+                      '#warriordash #seattle',
+                      '#warriordash')
+
+#Tough Mudder
+# @ToughMudder
+toughmudder_tags <- c('#toughmudder AND #seattle')
+
+# @RunRocknRoll
+runrock_tags <- c('#RnRSEA')
+# should filter twitter text by seattle
+
+#Hot Choc Races
+hotchoc_tags <- c('#hc15k',
+                  '#hc5k',
+                  '#willrunforchocolate')
+
+misc_tags <- c(
+    '#GlobalRunningDay',
+    '#activerecovery',
+    '#fitchicks',
+    '#macros',
+    '#fitlife',
+    '#girlswholift',
+    '#girlswhosquat',
+    '#workhardplayhard',
+    '#workout',
+    '#saturdayworkout',
+    '#girlswhoworkout',
+    '#seattlerunning',
+    '#belltown'
+)
